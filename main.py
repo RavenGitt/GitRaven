58deb8f3752153bc8f827280c01fafc5a8200595
RavenGitt <202080114@psu.palawan.edu.ph>
2023-05-29 09:52:30 +0000

    test read working

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def cus_details(cus_id):
         cursor.close() 
         conn.close() 
 
-@app.route('/update', methods=['PUT'])
+@app.route('/updat', methods=['PUT'])
 def update_cus():
     try:
         _json = request.json
